416271abfbb3c2634969ac314eaaec7c67ed9542
mellava <6126925+mellava@users.noreply.github.com>
2023-04-10 16:26:34 -0700

    Update App, VideoList, and VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,32 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '/src/data/exampleVideoData.js';
+
+
+var App = () => {
+
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em> <Search/></em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em><VideoPlayer/></em> view goes here</h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><h5><em><VideoList videos = {exampleVideoData}/></em> view goes here</h5></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
